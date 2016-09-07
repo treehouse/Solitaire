7a2bca7c843a251a136c3084630018f9fb36ea20
Ben <bdeitch@teamtreehouse.com>
2016-09-06 17:46:46 -0700

    Creating the counter app using Anko instead.

--- app/src/main/java/com/teamtreehouse/solitaire/MainActivity.kt
+++ app/src/main/java/com/teamtreehouse/solitaire/MainActivity.kt
@@ -2,18 +2,28 @@ package com.teamtreehouse.solitaire
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.button
+import org.jetbrains.anko.onClick
+import org.jetbrains.anko.relativeLayout
+import org.jetbrains.anko.textView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         var counter = 0
-        counterButton.setOnClickListener {
-            counter++
-            counterTextView.text = counter.toString()
+        relativeLayout {
+            val counterTextView = textView {
+                text = "0"
+                textSize = 24f
+            }
+            button {
+                onClick {
+                    counter++
+                    counterTextView.text = counter.toString()
+                }
+            }
         }
     }
 }
