78ce4557198ffd3eb67c83a7dbdc2d30681e84a4
Ben <bdeitch@teamtreehouse.com>
2016-09-06 18:00:33 -0700

    Finish out the first row of the layout.

--- app/src/main/java/com/teamtreehouse/solitaire/MainActivity.kt
+++ app/src/main/java/com/teamtreehouse/solitaire/MainActivity.kt
@@ -13,8 +13,17 @@ class MainActivity : AppCompatActivity() {
         val cardHeight = cardWidth * 190 / 140
 
         verticalLayout {
-            linearLayout {
+            leftPadding = dip(4)
+            rightPadding = dip(4)
+            topPadding = dip(8)
 
+            linearLayout {
+                imageView(imageResource = R.drawable.cardback_green5).lparams(cardWidth, cardHeight)
+                imageView(imageResource = R.drawable.cardback_blue1).lparams(cardWidth, cardHeight)
+                view().lparams(cardWidth, 0)
+                for (i in 0..3) {
+                    imageView(imageResource = R.drawable.cardback_blue1).lparams(cardWidth, cardHeight)
+                }
             }
             linearLayout {
 
