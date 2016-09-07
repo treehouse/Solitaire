871586cb137e124d50f20a7c52ebf8e526bbea56
Ben <bdeitch@teamtreehouse.com>
2016-09-06 18:06:52 -0700

    Created the DeckView class.

---
+++ app/src/main/java/com/teamtreehouse/solitaire/DeckView.kt
@@ -0,0 +1,27 @@
+package com.teamtreehouse.solitaire
+
+import android.content.Context
+import android.view.ViewManager
+import android.widget.ImageView
+import org.jetbrains.anko.custom.ankoView
+import org.jetbrains.anko.imageResource
+import org.jetbrains.anko.onClick
+
+
+class DeckView(context: Context) : ImageView(context) {
+    init {
+        imageResource = cardBackDrawable
+        onClick {
+            GamePresenter.onDeckTap()
+        }
+    }
+
+    fun update() {
+        val cards = GameModel.deck.cardsInDeck
+        imageResource = if (cards.size > 0) cardBackDrawable else emptyPileDrawable
+    }
+}
+
+val DECKVIEW_FACTORY = { ctx: Context -> DeckView(ctx) }
+fun ViewManager.deckView(init: DeckView.() -> Unit = {}) =
+        ankoView(DECKVIEW_FACTORY, 0, init)
--- app/src/main/java/com/teamtreehouse/solitaire/MainActivity.kt
+++ app/src/main/java/com/teamtreehouse/solitaire/MainActivity.kt
@@ -4,10 +4,15 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import org.jetbrains.anko.*
 
-class MainActivity : AppCompatActivity() {
+val cardBackDrawable = R.drawable.cardback_green5
+val emptyPileDrawable = R.drawable.cardback_blue1
 
+class MainActivity : AppCompatActivity(), GameView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        GamePresenter.setGameView(this)
+        GameModel.resetGame()
 
         val cardWidth = (displayMetrics.widthPixels - dip(8)) / 7
         val cardHeight = cardWidth * 190 / 140
@@ -18,16 +23,20 @@ class MainActivity : AppCompatActivity() {
             topPadding = dip(8)
 
             linearLayout {
-                imageView(imageResource = R.drawable.cardback_green5).lparams(cardWidth, cardHeight)
-                imageView(imageResource = R.drawable.cardback_blue1).lparams(cardWidth, cardHeight)
+                deckView().lparams(cardWidth, cardHeight)
+                imageView(imageResource = emptyPileDrawable).lparams(cardWidth, cardHeight)
                 view().lparams(cardWidth, 0)
                 for (i in 0..3) {
-                    imageView(imageResource = R.drawable.cardback_blue1).lparams(cardWidth, cardHeight)
+                    imageView(imageResource = emptyPileDrawable).lparams(cardWidth, cardHeight)
                 }
             }
             linearLayout {
 
             }
         }
+    }
+
+    override fun update(gameModel: GameModel) {
+
     }
 }
